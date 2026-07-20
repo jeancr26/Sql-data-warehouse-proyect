2115f839f1dfd3d8edc897b834537ea4e055b84a
jeancr26 <jeancr26@gmail.com>
2026-07-20 13:05:49 -0400

    Update ddl.bronze.sql

--- scripts/bronze/ddl.bronze.sql
+++ scripts/bronze/ddl.bronze.sql
@@ -20,10 +20,10 @@ CREATE TABLE bronze.crm_cust_info
 	cst_id INT,
 	cst_key NVARCHAR(50),
 	cst_firstname NVARCHAR(50),
-	cst_lasttname NVARCHAR(50),
+	cst_lastname NVARCHAR(50),
 	cst_material_status NVARCHAR(50),
 	cst_gndr NVARCHAR(50),
-	cst_Create_date DATE
+	cst_create_date DATE
 );
 
 IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL
